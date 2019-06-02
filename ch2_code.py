6adc56a9514f665e99f4e00d2950b06b1367304f
hye <hye@keen.im>
2019-06-02 20:34:22 +1000

    added all completed chapters

--- ch2_code.py
+++ ch2_code.py
@@ -195,7 +195,7 @@ class AdalineGD():
 		return np.where(self.activation(X) >= 0.0, 1, -1)
 
 
-# %% Make random data
+# %% Make random data - extra exercise, not in book
 # different ways the data frames work....
 rnd_col = rand.sample(range(1,100),10)
 rnd_col2 = rand.sample(range(1,100),10)
@@ -219,7 +219,130 @@ y = [rand.choice([-1, 1]) for _ in range(10)]
 list(zip(X, y))   # makes each row a tuple of all the X vars accompanied by the y output
 
 
+# %% Making standardised data
+X_std = np.copy(X)  # copy of X
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()  # manually normalise X
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
+# %% Adeline - Stochastic Gradient Descent
+from numpy.random import seed
+
+class AdalineSGD(object):
+	"""ADAptive LInear NEuron classifier.
+
+	Parameters
+	------------
+	eta : float
+	Learning rate (between 0.0 and 1.0)
+	n_iter : int
+	Passes over the training dataset.
+
+	Attributes
+	-----------
+	w_ : 1d-array
+	Weights after fitting.
+	errors_ : list
+	Number of misclassifications in every epoch.
+	shuffle : bool (default: True)
+	Shuffles training data every epoch
+	if True to prevent cycles.
+	random_state : int (default: None)
+	Set random state for shuffling
+	and initializing the weights.
+
+	"""
+	def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.w_initialized = False
+		self.shuffle = shuffle
+		if random_state:
+			seed(random_state)
+
+	def fit(self, X, y):
+		""" Fit training data.
+		Parameters
+		----------
+		X : {array-like}, shape = [n_samples, n_features]
+		Training vectors, where n_samples
+		is the number of samples and
+		n_features is the number of features.
+		y : array-like, shape = [n_samples]
+		Target values.
+
+		Returns
+		-------
+		self : object
+		"""
+		self._initialize_weights(X.shape[1])
+		self.cost_ = []
+		for i in range(self.n_iter):
+			if self.shuffle:
+				X, y = self._shuffle(X, y)
+			cost = []
+			for xi, target in zip(X, y):
+				cost.append(self._update_weights(xi, target))
+			avg_cost = sum(cost)/len(y)
+			self.cost_.append(avg_cost)
+		return self
+
+	def partial_fit(self, X, y):
+		"""Fit training data without reinitializing the weights"""
+		if not self.w_initialized:
+			self._initialize_weights(X.shape[1])
+		if y.ravel().shape[0] > 1:
+			for xi, target in zip(X, y):
+				self._update_weights(xi, target)
+		else:
+			self._update_weights(X, y)
+		return self
+
+	def _shuffle(self, X, y):
+		"""Shuffle training data"""
+		r = np.random.permutation(len(y))
+		return X[r], y[r]
+
+	def _initialize_weights(self, m):
+		"""Initialize weights to zeros"""
+		self.w_ = np.zeros(1 + m)
+		self.w_initialized = True
+
+	def _update_weights(self, xi, target):
+		"""Apply Adaline learning rule to update the weights"""
+		output = self.net_input(xi)
+		error = (target - output)
+		self.w_[1:] += self.eta * xi.dot(error)
+		self.w_[0] += self.eta * error
+		cost = 0.5 * error**2
+		return cost
+
+	def net_input(self, X):
+		"""Calculate net input"""
+		return np.dot(X, self.w_[1:]) + self.w_[0]
+
+	def activation(self, X):
+		"""Compute linear activation"""
+		return self.net_input(X)
+
+	def predict(self, X):
+		"""Return class label after unit step"""
+		return np.where(self.activation(X) >= 0.0, 1, -1)
+
+# %%
+
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+plt.show()
 
 
 
--- ch3_code.py
+++ ch3_code.py
@@ -10,10 +10,11 @@ Chapter 3
 @author: yehan
 """
 
-from sklearn import datasets
 import numpy as np
+import matplotlib.pyplot as plt
 
 # %% Load/inspect data
+from sklearn import datasets
 
 iris = datasets.load_iris()  # didn't know you could get this for free...
 X = iris.data[:, [2, 3]]
@@ -53,13 +54,14 @@ roc_curve(y_test, y_prd)
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
-	'''Testing:
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02, fig=0):
+	'''Testing variables:
 	classifier = Perceptron()
 	classifier.fit(X, y)
 	resolution=0.02
 	'''
-
+	# added extra param to allow for multiple flots
+	plt.figure(fig)
 	# setup marker generator and color map
 	markers = ('s', 'x', 'o', '^', 'v')  # pre-define types of markers and colours to plot
 	colours = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -92,10 +94,10 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 				    alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
 
 # %% plotting
-X_all_std = np.vstack((X_train_std, X_test_std))
+X_combined_std = np.vstack((X_train_std, X_test_std))
 y_all = np.hstack((y_train, y_test))
-plot_decision_regions(X=X_all_std, y=y_all,
-					  classifier=ppn, test_idx=range(105, 150))
+plot_decision_regions(X=X_combined_std, y=y_all,
+					  classifier=ppn, test_idx=range(105, 150), fig=0)
 plt.xlabel('petal length [std]')
 plt.ylabel('petal width [std]')
 plt.legend(loc='lower right')
@@ -114,8 +116,198 @@ plt.yticks([0.0, 0.5, 1.0])
 plt.ylim(-0.1, 1.1)
 plt.xlabel('z')
 plt.ylabel('$\phi (z)$') # maths phi
+plt.show()  # not necessary for ipython
+
+# %% logistic regression with scikit-learn
+
+from sklearn.linear_model import LogisticRegression
+
+mod_lrm = LogisticRegression(C=1000.0, random_state=0)  # model: Logistic regression model
+mod_lrm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_all, classifier=mod_lrm, test_idx=range(105, 150), fig=1)
+plt.xlabel('petal length [std.]')
+plt.ylabel('petal width [std.]')
+plt.legend(loc='lower center')
+plt.show()  # not necessary for ipython
+
+mod_lrm.predict_proba(X_test_std[0,:].reshape(1, -1))  # gives the prd. probabilities for each class
+
+# %% applying regularisation to logistic regression
+
+weights, params = [], []
+for c in range(-5, 5):  # cant use np.arange here, which returns np.ints, can't be raised to negative powers
+	mod_lrr = LogisticRegression(C=10**c, random_state=0)  # model: log regression w/ regularsation
+	mod_lrr.fit(X_train_std, y_train)
+	print(mod_lrr.coef_)  # for 3 class output, 3 models get fitted with logistic reg.
+	# to simulate 3x 1 vs all models. good interview question
+	weights.append(mod_lrr.coef_[1])  # only gets coeff for the model of the 2nd class
+	params.append(10**c)  # ** is ^, raising to power
+
+weights = np.array(weights)  # converts array of tuples into array of lists, which can be sliced
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.xscale('log')
+plt.xlabel('C')
+plt.ylabel('weight coefficient')
+plt.legend(loc='lower left')
 
 # %%
+
+from sklearn.svm import SVC
+
+mod_svm = SVC(kernel='linear', C=1, random_state=0)
+mod_svm.fit(X_train_std, y_train)
+
+sum(mod_svm.predict(X_test_std)==y_test)/len(y_test)  # accuracy
+
+plot_decision_regions(X_combined_std, y_all, classifier=mod_svm, test_idx=range(105,150),fig=2)
+plt.xlabel('petal len')
+plt.ylabel('petal wid')
+plt.legend(loc='bottom left')  # doesn't work, but gives you actual possible choices
+plt.legend()
+plt.axis('off')
+
+
+# %% Stochastic GD classifiers - use the .partial_fit() method
+from sklearn.linear_model import SGDClassifier
+
+ppn = SGDClassifier(loss='perceptron')
+lr = SGDClassifier(loss='log')
+svm = SGDClassifier(loss='hinge')
+
+
+# %% non linear SVM - make fake data
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)  # random array of 200 rows x 200 cols
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)  # used for comparing multiple arrays elementwise
+y_xor = np.where(y_xor, 1, -1)  #converts a bool array into a -1 or 1 array
+
+plt.figure(1)  # used to choose which figure/plot to draw on
+plt.scatter(X_xor[y_xor==1,0], X_xor[y_xor==1,1], color='blue', marker='x', label='1')  # plot 1's
+plt.scatter(X_xor[y_xor==-1,0], X_xor[y_xor==-1,1], color='red', marker='s', label='1')  # plot 1's
+plt.ylim(-3,3)
+plt.legend()
+
+
+# %% RBF/Gaussian Kernel for SVM on non-linear values
+from sklearn.svm import SVC
+mod_svm_rbf = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)  # rbf is acutally default kernel
+# gamma is param controlling similarity measure of kernel
+mod_svm_rbf.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=mod_svm_rbf, fig=2)
+plt.legend()
+
+# %% SVM on Iris data
+mod_svm_iris = SVC(kernel='rbf', random_state=0, gamma=0.2, C=1.0)
+mod_svm_iris.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_all, mod_svm_iris, test_idx=range(105, 150), fig=3)
+plt.legend()
+
+# %% SVM modified gamma param
+
+mod_svm_iris = SVC(kernel='rbf', random_state=0, gamma=100, C=1.0)
+mod_svm_iris.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_all, mod_svm_iris, test_idx=range(105,150), fig=4)
+plt.legend()
+
+
+# %% Trees - Node Cost functions
+# %reset  # ipython magic function to clear workspace
+
+import numpy as np
+import matplotlib.pyplot as plt
+def gini(p):
+	return (p) * (1 - (p)) + (1 - p) * (1 - (1 - p))
+
+def entropy(p):
+	return -p * np.log2(p) - (1 - p) * np.log2(1 - p)
+
+def error(p):  # misclassification
+	return 1 - np.max([p, 1 - p])
+
+x = np.arange(0.0, 1.0, 0.01) # make a vector from 0 to 1 with steps 0.01
+#range(0.0, 1.0, 0.01))  # doesn't work with floats
+ent = [[p, entropy(p)] if p != 0 else None for p in x]  # floating point maths is broken lol
+ent = [entropy(p) if p != 0 else None for p in x]  # floating point maths is broken lol
+sc_ent = [e * 0.5 if e else None for e in ent]  # scaled version of entropy
+err = [error(i) for i in x]  # misclassification rate
+
+# make a plot of all the stuff
+fig = plt.figure(5)
+ax = plt.subplot(111)
+for i, lab, ls, c in zip([ent, sc_ent, gini(x), err],
+							 ['Entropy', 'Entropy (scaled)', 'Gini', 'Misclassification'],
+							 ['-', '-', '--', '-.'],
+							 ['black', 'lightgray', 'red', 'green', 'cyan']):
+	line = ax.plot(x, i ,label=lab, linestyle=ls, lw=2, color=c)
+	ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=False)
+	ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+	ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+	plt.ylim([0, 1.1])
+	plt.xlabel('p(i=1)')
+	plt.ylabel('Impurity index')
+	#plt.show()
+
+
+# %% decision trees
+
+from sklearn.tree import DecisionTreeClassifier
+
+mod_tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+mod_tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))  # works on 1d arrays (i.e. no explicit columns)
+#y_combined = np.vstack((y_train.reshape(-1, 1), y_test.reshape(-1, 1)))  # same as above
+# except has an explicit column
+plot_decision_regions(X_combined, y_combined, classifier=mod_tree,
+						 test_idx=range(105,150), fig=8)
+plt.xlabel('petal length cm')
+plt.ylabel('petal width cm')
+plt.legend(loc='lower right')
+plt.title('Iris flowers - Tree classifier')
+
+
+# %% Extra - tree plotting
+from sklearn.tree import export_graphviz
+
+# Note graphviz
+export_graphviz(mod_tree, out_file='tree.dot',
+			 feature_names=['petal length', 'petal width'])
+
+
+
+# %% Aside/extra stuff
+
+# np.int can't do negative powers
+
+%timeit for x in range(int(-5E5), int(5E5)): x ** 2
+%timeit for x in np.arange(int(-5E5), int(5E5)): x ** 2
+# np won't work
+%timeit for x in range(-5000, 5000): x ** x
+%timeit for x in np.arange(-5000, 5000): x ** x
+
+
+# fit logistic regression on random binary classes
+import random as rand
+
+y_rng = [rand.choice([-1, 1]) for _ in range(0, 105)]
+
+mod_rand = LogisticRegression(C=10, random_state=0)
+mod_rand.fit(X_train_std, y_rng)
+mod_rand.coef_  # should only have 1 pair of coefs.
+plot_decision_regions(X_train_std, y_rng, mod_rand)
+
+
+# Plot using extra utility
+from adspy_shared_utilities import plot_decision_tree
+
+
+
+
+
+
+
+
 
 
 
